72069fe4eba6431d145a9f228e8f878b969c4cf4
LujainAlfaqir <121673299+LujainAlfaqir@users.noreply.github.com>
2023-01-13 13:16:34 +0300

    test

---
+++ Final_Project/Assets/scripts/EnemyA.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
